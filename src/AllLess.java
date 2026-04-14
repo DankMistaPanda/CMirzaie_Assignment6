a73fe53597644d9dd19d50c077ed33b47d685321
DankMistaPanda <70719496+DankMistaPanda@users.noreply.github.com>
2026-04-13 20:48:40 -0500

    Initial Commit For Question 5

---
+++ src/AllLess.java
@@ -0,0 +1,19 @@
+/*
+5 (code) All Less [10 points]
+Given a lexicographical max heap of Strings s, and an integer x, write an algorithm to find all strings in
+the heap whose length is less than x.
+Input Format
+An array representation of a heap of strings s, where s[i] will be a String and s can be empty
+Constraints
+• 0 ≤ |𝑠| ≤ 10!, where |𝒔| is the length of the strings array
+• S[i] can be an empty String or null.
+Output Format
+Print the list of strings.
+Sample Input
+s = ["zero", " size", "nutella", "jojo", "luna", "isse", "astor", "as", "entretien", “”, "cal"]
+x = 3
+Sample Output
+[ “as” ]
+ */
+public class AllLess {
+}
