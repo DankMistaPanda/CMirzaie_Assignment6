cc6241c4ed46a6a0a57ebc3ae6fe13d2f28664ea
DankMistaPanda <70719496+DankMistaPanda@users.noreply.github.com>
2026-04-13 20:45:39 -0500

    Initial Commit For Question 6

---
+++ src/DeleteElements.java
@@ -0,0 +1,13 @@
+/*
+6 (code) BR Tree, Delete Elements in Range [15 points]
+Given a BR Tree brt, and two integers a and b, write a function to delete all the nodes that have values
+within the inclusive range [𝑎, 𝑏] from the tree.
+Note: You Must use a Node implementation of a BR Tree and the resulting trees should remain valid BR
+Trees.
+Examples: Suppose a BR Tree has keys: {10, 19, 20, 30, 42, 55, 77}
+A call to inRange(15, 20) the tree would have the keys {10, 30, 42, 55, 77}
+A call to inRange(0, 2) the tree would have the keys {10, 19, 20, 30, 42, 55, 77}
+A call to inRange(25, 60) the tree would have the keys {10, 19, 20, 77}
+ */
+public class DeleteElements {
+}
